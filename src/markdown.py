34351f6c2b656b44652d6dc0f30557a5cc0d46b2
GuilhermeNeto <eng.guilhermeneto@hotmail.com>
2026-02-25 17:57:37 -0300

    Correct markdown generation for destinations
    Fix loop to iterate over single destination instead of set.

--- src/markdown.py
+++ src/markdown.py
@@ -92,9 +92,8 @@ def generate_moves_list(board):
     markdown += "|  FROM  | TO (Just click a link!) |\n"
     markdown += "| :----: | :---------------------- |\n"
 
-    # CORREÇÃO AQUI: source é a casa de origem, destinations é o CONJUNTO de destinos
-    for source, destinations in sorted(moves_dict.items()):
-        markdown += "| **" + source + "** | " + create_issue_link(source, destinations) + " |\n"
+    for source,dest in sorted(moves_dict.items()):
+        markdown += "| **" + source + "** | " + create_issue_link(source, dest) + " |\n"
 
     return markdown
 
